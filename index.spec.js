8ea4d60ca44f342e10c862eaa17ef49c33ff19be
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-04-12 09:07:50 -0700

    Built my POST endpoint and 2 tests working on a 3rd

--- index.spec.js
+++ index.spec.js
@@ -18,9 +18,33 @@ describe('Server JS', () => {
       });
     });
 
-    describe('/games get', () => {
+    describe("/games POST route", () => {
+        it("returns status 201 upon success", async () => {
+          let response = await request(server)
+            .post("/games")
+            .send({ title: "GTA", genre: "RPG" });
+    
+          expect(response.status).toBe(201);
+        });
+
+        it("successfully adds a new game to the games db", async () => {
+          await request(server)
+            .post("/games")
+            .send({ title: "PUBG", genre: "FPS" });
+          let games = await db("games").where({ title: "PUBG" });
+          expect(games.length).toBe(1);
+    
+          await request(server)
+            .post("/games")
+            .send({ title: "Fortnite", genre: "TPS" });
+          games = await db("games");
+          expect(games).toHaveLength(2);
+        });
+    
         
+    
     });
+    
 
     
 });
--- server.js
+++ server.js
@@ -9,4 +9,22 @@ server.get("/", (req, res) => {
 });
 
 
+server.post("/games", (req, res) => {
+    const game = req.body;
+  
+    !game.title || !game.genre
+        ? res.status(422)
+            .json({message:"Please fill out a title and genre before submitting"})
+        : db("games")
+            .where({ title: game.title })
+            .then(dbGame => {
+            dbGame && dbGame.length
+                ? res.status(405).json({ message: "That game already exists." })
+                : db("games")
+                    .insert(game)
+                    .then(id => res.status(201).json(id))
+                    .catch(err => res.status(500).json(err));
+            });
+  });
+
 module.exports = server;
